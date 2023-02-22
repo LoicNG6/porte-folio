9226a7ff36f1fc246a46e76980cab58ad4acc59a
LoicNG6 <loicnguessie@hotmail.com>
2023-02-22 23:44:08 +0100

    new settings fixed

--- resources/js/components/home/Home.vue
+++ resources/js/components/home/Home.vue
@@ -1,151 +1,176 @@
 <template>
-  <v-container fluid v-if="$route.name == 'home'" class="pa-0">
-    <v-row justify="center" class="my-4">
-      <information-card-tool style="color: whitesmoke">
-        <template v-slot:card-content>
-          <v-card-title>{{ $t("home.introduction_title") }}</v-card-title>
-          <v-divider color="#c8b4b4"></v-divider>
-          <v-card-text>
-            <v-container>
-              <v-row class="text" style="text-align: justify">{{
-                $t("home.introduction")
-              }}</v-row>
-            </v-container>
-          </v-card-text>
-        </template>
-      </information-card-tool>
-      <information-card-tool>
-        <template v-slot:card-content>
-          <v-card-title>
-            <v-row justify="space-between">
-              <v-col cols="auto">
-                {{ $t("home.tips_title") }}
-              </v-col>
-              <v-col cols="auto">
-                <v-icon light color="yellow"> mdi-lightbulb-on </v-icon>
-              </v-col>
-            </v-row>
-          </v-card-title>
-          <v-divider color="#c8b4b4"></v-divider>
-          <v-card-text>
-            <v-container>
-              <v-row class="text" style="text-align: justify">{{
-                $t("home.tips")
-              }}</v-row>
-            </v-container>
-          </v-card-text>
-        </template>
-      </information-card-tool>
-    </v-row>
-    <v-row align-content="center" justify="center" class="text my-8">
-      <v-sheet class="mx-auto" max-width="1100" color="#ffffff00">
-        <v-slide-group
-          style="color: white"
-          v-model="model"
-          class="pa-4"
-          prev-icon="mdi-arrow-left-bold"
-          next-icon="mdi-arrow-right-bold"
-          show-arrows
-        >
-          <v-slide-group-item v-for="(topic, index) in topics" :key="index">
-            <v-card
-              class="mx-6 text card-bg"
-              width="270"
-              rounded="xl"
-              height="250"
-            >
-              <v-container fluid>
-                <v-row justify="center">
-                  <v-col cols="auto" class="text">
-                    {{ topic.title[$i18n.locale] }}
-                  </v-col>
-                </v-row>
-                <v-row><v-divider color="white"></v-divider></v-row>
-                <v-row justify="center" style="height: 100px" class="my-4">
-                  <v-col cols="12">
-                    {{ topic.description[$i18n.locale] }}
-                  </v-col>
-                </v-row>
-                <v-row justify="center">
-                  <v-col cols="8" justify-center style="text-align: center">
-                    <v-btn
-                      @click="goToSection(topic.id, topic.title[$i18n.locale])"
-                      width="100"
-                      rounded="pill"
-                      class="btn-bg"
+    <v-container fluid v-if="$route.name == 'home'" class="pa-0">
+        <v-row justify="center" class="my-4">
+            <information-card-tool style="color: whitesmoke">
+                <template v-slot:card-content>
+                    <v-card-title>{{
+                        $t("home.introduction_title")
+                    }}</v-card-title>
+                    <v-divider color="#c8b4b4"></v-divider>
+                    <v-card-text>
+                        <v-container>
+                            <v-row class="text" style="text-align: justify">{{
+                                $t("home.introduction")
+                            }}</v-row>
+                        </v-container>
+                    </v-card-text>
+                </template>
+            </information-card-tool>
+            <information-card-tool>
+                <template v-slot:card-content>
+                    <v-card-title>
+                        <v-row justify="space-between">
+                            <v-col cols="auto">
+                                {{ $t("home.tips_title") }}
+                            </v-col>
+                            <v-col cols="auto">
+                                <v-icon light color="yellow">
+                                    mdi-lightbulb-on
+                                </v-icon>
+                            </v-col>
+                        </v-row>
+                    </v-card-title>
+                    <v-divider color="#c8b4b4"></v-divider>
+                    <v-card-text>
+                        <v-container>
+                            <v-row class="text" style="text-align: justify">{{
+                                $t("home.tips")
+                            }}</v-row>
+                        </v-container>
+                    </v-card-text>
+                </template>
+            </information-card-tool>
+        </v-row>
+        <v-row align-content="center" justify="center" class="text my-8">
+            <v-sheet class="mx-auto" max-width="1100" color="#ffffff00">
+                <v-slide-group
+                    style="color: white"
+                    v-model="model"
+                    class="pa-4"
+                    prev-icon="mdi-arrow-left-bold"
+                    next-icon="mdi-arrow-right-bold"
+                    show-arrows
+                >
+                    <v-slide-group-item
+                        v-for="(topic, index) in topics"
+                        :key="index"
                     >
-                      <v-icon class="btn-text" size="x-large">
-                        mdi-eye-outline
-                      </v-icon>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card>
-          </v-slide-group-item>
-        </v-slide-group>
-      </v-sheet>
-    </v-row>
-  </v-container>
-  <router-view v-else />
+                        <v-card
+                            class="mx-6 text card-bg"
+                            width="270"
+                            rounded="xl"
+                            height="250"
+                        >
+                            <v-container fluid>
+                                <v-row justify="center">
+                                    <v-col cols="auto" class="text">
+                                        {{ topic.title[$i18n.locale] }}
+                                    </v-col>
+                                </v-row>
+                                <v-row
+                                    ><v-divider color="white"></v-divider
+                                ></v-row>
+                                <v-row
+                                    justify="center"
+                                    style="height: 100px"
+                                    class="my-4"
+                                >
+                                    <v-col cols="12">
+                                        {{ topic.description[$i18n.locale] }}
+                                    </v-col>
+                                </v-row>
+                                <v-row justify="center">
+                                    <v-col
+                                        cols="8"
+                                        justify-center
+                                        style="text-align: center"
+                                    >
+                                        <v-btn
+                                            @click="
+                                                goToSection(
+                                                    topic.id,
+                                                    topic.title[$i18n.locale]
+                                                )
+                                            "
+                                            width="100"
+                                            rounded="pill"
+                                            class="btn-bg"
+                                        >
+                                            <v-icon
+                                                class="btn-text"
+                                                size="x-large"
+                                            >
+                                                mdi-eye-outline
+                                            </v-icon>
+                                        </v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                        </v-card>
+                    </v-slide-group-item>
+                </v-slide-group>
+            </v-sheet>
+        </v-row>
+    </v-container>
+    <router-view v-else />
 </template>
 <script>
 import InformationCardTool from "./InformationCardTool.vue";
 export default {
-  components: {
-    InformationCardTool,
-  },
-  data: () => {
-    return {
-      topics: null,
-      model: null,
-      intro: null,
-      tips: null,
-    };
-  },
-  mounted() {
-    this.getTopics();
-    // console.log(this.$vuetify.display.sm);
-  },
-  computed: {
-    imageSrc() {
-      const image_paths = [];
-      this.topics.map((t) => {
-        image_paths.push({
-          section: t.title["fr"],
-          path: new URL(t.image, import.meta.url).href,
-        });
-      });
-      return image_paths;
+    components: {
+        InformationCardTool,
     },
-  },
-  methods: {
-    getTopics() {
-      axios
-        .get("/api/topics")
-        .then((res) => {
-          this.topics = res.data.data;
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+    data: () => {
+        return {
+            topics: null,
+            model: null,
+            intro: null,
+            tips: null,
+        };
     },
-    goToSection(topic_id, topic_name) {
-      this.$router.push({
-        name: "section",
-        params: { id: topic_id },
-        query: {
-          topic_title: topic_name,
-          dialog: !this.active_section_dialog,
+    mounted() {
+        this.getTopics();
+        // console.log(this.$vuetify.display.sm);
+    },
+    computed: {
+        imageSrc() {
+            const image_paths = [];
+            this.topics.map((t) => {
+                image_paths.push({
+                    section: t.title["fr"],
+                    path: new URL(t.image, import.meta.url).href,
+                });
+            });
+            return image_paths;
         },
-      });
     },
-  },
-  watch: {
-    "$i18n.locale": function () {
-      console.log("in home this.$i18n.locale = ", this.$i18n.locale);
-      this.$forceUpdate();
+    methods: {
+        getTopics() {
+            axios
+                .get("/api/topics")
+                .then((res) => {
+                    this.topics = res.data.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        goToSection(topic_id, topic_name) {
+            this.$router.push({
+                name: "section",
+                params: { id: topic_id },
+                query: {
+                    topic_title: topic_name,
+                    dialog: !this.active_section_dialog,
+                },
+            });
+        },
     },
-  },
+    watch: {
+        "$i18n.locale": function () {
+            console.log("in home this.$i18n.locale = ", this.$i18n.locale);
+            this.$forceUpdate();
+        },
+    },
 };
 </script>
--- resources/js/components/section/Sections.vue
+++ resources/js/components/section/Sections.vue
@@ -1,110 +1,121 @@
 <template>
-  <v-container class="text">
-    <!-- title of the page -->
-    <v-row>
-      <v-col cols="auto">
-        <span style="font-size: 1.5em"> {{ $route.query.topic_title }}</span>
-        <!-- for the traduction -->
-        <!-- {{
-            $t(
-              "section.description." +
-                $route.query.topic_title.substring(
-                  0,
-                  $route.query.topic_title.indexOf(".")
-                )
-            )
-          }}
-          élément 0 =
-          {{
-            $route.query.topic_title.substring(
-              0,
-              $route.query.topic_title.indexOf(".")
-            )
-          }} -->
-      </v-col>
-    </v-row>
+    <v-container class="text">
+        <!-- title of the page -->
+        <v-row>
+            <v-col cols="auto">
+                <span style="font-size: 1.5em">
+                    {{ $route.query.topic_title }}</span
+                >
+                <!-- for the traduction -->
+                <!-- {{
+						$t(
+							"section.description." +
+								$route.query.topic_title.substring(
+									0,
+									$route.query.topic_title.indexOf(".")
+								)
+						)
+					}}
+					élément 0 =
+					{{
+						$route.query.topic_title.substring(
+							0,
+							$route.query.topic_title.indexOf(".")
+						)
+					}} -->
+            </v-col>
+        </v-row>
 
-    <!-- Contain of the page -->
-    <v-row justify="start">
-      <v-col cols="auto">
-        <v-card
-          class="card-bg"
-          height="400"
-          width="500"
-          rounded="md"
-          elevation="5"
-        >
-          <img style="height: 50%; width: 50%" src="images/me/2001.jpg" />
-          <img style="height: 50%; width: 50%" src="images/me/java_code.png" />
-          <img style="height: 50%; width: 50%" src="images/me/lecture.jpg" />
-          <img style="height: 50%; width: 50%" src="images/me/travel.jpeg" />
-        </v-card>
-      </v-col>
-      <v-col cols="2">
-        <v-list class="pa-0" bg-color="transparent">
-          <v-list-item
-            v-for="information in sections_information"
-            :key="information.title"
-            style="border-left: solid 0.01em #ffffff85"
-            min-height="40px"
-          >
-            {{ information.title }} : {{ information.value }}
-          </v-list-item>
-        </v-list>
-      </v-col>
-    </v-row>
-    <v-row justify="center" >
-      <v-col cols="7">
-        <div style="text-align: justify">
-          description
-        </div>
-      </v-col>
-      <v-col cols="5" style="text-align: justify">
-        <div>
-          What I learned
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+        <!-- Contain of the page -->
+        <v-row justify="start">
+            <v-col cols="auto">
+                <v-card
+                    class="card-bg"
+                    height="400"
+                    width="500"
+                    rounded="md"
+                    elevation="5"
+                >
+                    <img
+                        style="height: 50%; width: 50%"
+                        src="images/me/2001.jpg"
+                    />
+                    <img
+                        style="height: 50%; width: 50%"
+                        src="images/me/java_code.png"
+                    />
+                    <img
+                        style="height: 50%; width: 50%"
+                        src="images/me/lecture.jpg"
+                    />
+                    <img
+                        style="height: 50%; width: 50%"
+                        src="images/me/travel.jpeg"
+                    />
+                </v-card>
+            </v-col>
+            <v-col cols="2">
+                <v-list class="pa-0" bg-color="transparent">
+                    <v-list-item
+                        v-for="information in sections_information"
+                        :key="information.title"
+                        style="border-left: solid 0.01em #ffffff85"
+                        min-height="30px"
+                    >
+                        {{ information.title }} : {{ information.value }}
+                    </v-list-item>
+                </v-list>
+            </v-col>
+        </v-row>
+        <v-row justify="center">
+            <v-col cols="6" class="bs" style="text-align: justify">
+                <span>Title</span>
+                <span>Contain</span>
+            </v-col>
+            <v-col cols="6" class="bs" style="text-align: justify">
+                <div>What I learned</div>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 <script>
 export default {
-  props: {
-    id: [Number, String],
-    dialog: Boolean,
-  },
-  data: () => {
-    return {
-      sections: null,
-      sections_information: [
-        { title: "Date", value: null },
-        { title: "Location", value: null },
-        { title: "Context", value: null },
-        { title: "Team", value: null },
-      ],
-    };
-  },
-  computed: {
-    active: {
-      get() {
-        return this.dialog;
-      },
+    props: {
+        id: [Number, String],
+        dialog: Boolean,
     },
-  },
-  mounted() {
-    this.getSections();
-  },
-  methods: {
-    getSections() {
-      axios
-        .get("/api/sections/" + this.id)
-        .then((res) => {
-          this.sections = res.data.data;
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+    data: () => {
+        return {
+            sections: null,
+            sections_information: [
+                { title: "Date", value: null },
+                { title: "Location", value: null },
+                { title: "Context", value: null },
+                { title: "Team", value: null },
+            ],
+        };
     },
-  },
+    computed: {
+        active: {
+            get() {
+                return this.dialog;
+            },
+        },
+    },
+    mounted() {
+        this.getSections();
+    },
+    methods: {
+        getSections() {
+            axios
+                .get("/api/sections/" + this.id)
+                .then((res) => {
+                    this.sections = res.data.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+    },
 };
 </script>
