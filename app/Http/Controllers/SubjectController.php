46a5dfe8abf969fc9a2539306ac8aca056c2c736
LoicNG6 <loicnguessie@hotmail.com>
2023-04-05 00:10:08 +0200

    extract and arrange subject and subject contents : done

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -25,24 +25,35 @@ class SubjectController extends Controller
 
     public function show(int $id)
     {
-        $subject = DB::table("subjects")->where("section_id", $id)->get();
+        $subject_info = DB::table("subjects")->where("section_id", $id)->get();
         $subject_ids = [];
 
-        foreach ($subject as $subject_field) {
-            $subject_field->team = json_decode($subject_field->team);
-            $subject_field->started_at = json_decode($subject_field->started_at);
-            $subject_field->ended_at = json_decode($subject_field->ended_at);
-            array_push($subject_ids, $subject_field->id);
+        foreach ($subject_info as $info) {
+            $info->team = json_decode($info->team);
+            $info->started_at = json_decode($info->started_at);
+            $info->ended_at = json_decode($info->ended_at);
+            array_push($subject_ids, $info->id);
         }
 
-        $subject_contents = (new SubjectContentController)->show($subject_ids);
-        
+        $subject_contents = (new SubjectContentController)->show($subject_ids)["data"]->all();
+        $subject_info_contents = [];
+
+        foreach ($subject_info as $info) {
+            $temp = [];
+            foreach ($subject_contents as $content) {
+                if ($content->subject_id == $info->id) {
+                    array_push($temp, $content);
+                }
+            }
+            array_push($subject_info_contents, [
+                "info" => $info,
+                "contents" => $temp
+            ]);
+        }
+
         return [
             "status" => 200,
-            "data" => [
-                "contents" => $subject_contents["data"],
-                "info" => $subject[0],
-            ]
+            "data" => $subject_info_contents
         ];
     }
 }
