1745551afe91d886218e01b6db99048f62f5f4c1
LoicNG6 <loicnguessie@hotmail.com>
2023-04-15 19:20:05 +0200

    subject card of personal project :done

--- resources/js/components/tools/section/SectionSubjectContent.vue
+++ resources/js/components/tools/section/SectionSubjectContent.vue
@@ -1,7 +1,6 @@
 <template>
     <v-col cols="4" v-if="isLeftSubject">
         <v-card class="card-bg text" height="100%" width="100%" rounded="md">
-            subject images
         </v-card>
     </v-col>
     <v-col>
@@ -43,9 +42,45 @@
         </v-col>
     </v-col>
     <v-col cols="4" v-if="!isLeftSubject">
-        <v-card class="card-bg text" height="100%" width="100%" rounded="md">
-            subject images
-        </v-card>
+        <template v-if="(locale_subject.info.id = 13)">
+            <v-card
+                v-for="(skillType, index) in skills"
+                :key="index"
+                class="card-test-bg text mb-4"
+                width="100%"
+                rounded="lg"
+            >
+                <v-card-title>{{ index }}</v-card-title>
+                <v-divider color="#ffffff"></v-divider>
+                <v-list bg-color="transparent" class="px-4">
+                    <v-list-item
+                        v-for="skill in skillType"
+                        :key="skill"
+                        :style="
+                            skill != skillType.slice(-1)
+                                ? 'border-bottom: solid 0.01em #ffffff35'
+                                : ''
+                        "
+                        class="px-0"
+                    >
+                        <span>{{ skill }}</span>
+                        <template v-slot:append>
+                            <img :src="iconPath" />
+                        </template>
+                    </v-list-item>
+                </v-list>
+            </v-card>
+        </template>
+        <template v-else>
+            <v-card
+                class="card-bg text"
+                height="100%"
+                width="100%"
+                rounded="md"
+            >
+                <template> locale_subject.info.image </template>
+            </v-card>
+        </template>
     </v-col>
 </template>
 <style scoped>
@@ -71,6 +106,30 @@ export default {
         isLeftSubject: {
             type: Boolean,
             required: true,
+        },
+    },
+    data: () => {
+        return {
+            skills: {
+                "Soft skills": [
+                    "Writing specification documents and documentations",
+                    "Make presentations + Doing formations",
+                    "Being efficient + Being disciplined",
+                ],
+                "Hard skills": [
+                    "Front = Vuejs 3, Vuetify 3",
+                    "Back = Php 8, Mysql 8",
+                    "Using of Test domain driven",
+                ],
+            },
+        };
+    },
+    computed: {
+        iconPath() {
+            return new URL(
+                "../../../../sass/assets/checked-icon.svg",
+                import.meta.url
+            ).href;
         },
     },
 };
